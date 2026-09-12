1e0e42b390a6a0007600f7b7484950f6574e1368
John Lloyd Vargas <vargasjohnlloyd139@gmail.com>
2026-09-12 08:15:32 +0800

    fetch movies

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [items, setItems] = useState([]);
 
-export default Home
+  useEffect(() => {
+    async function fetchItems() {
+      try {
+        const response = await fetch("/data.json");
+        const data = await response.json();
+        setItems(data.titles);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchItems();
+  }, []);
+
+  return (
+    <>
+      <Helmet>
+        <title>Netflix.budget Edition</title>
+      </Helmet>
+      <div className="HomePage">
+        Home
+        {items.map((item) => {
+          return (
+            <>
+              <div className="ewan" key={item.id}>
+                <p>{item.title}</p>
+                <Link to={`/Movie/${item.id}`} state={{ item }}>
+                  View Movie Details
+                </Link>
+              </div>
+              ;
+            </>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Home;
