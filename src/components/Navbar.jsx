af3633f0e9ac58d2b1bf7f37eb7ff6e5151bb96c
John Lloyd Vargas <vargasjohnlloyd139@gmail.com>
2026-09-12 08:39:35 +0800

    design navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { NavLink } from "react-router-dom";
 import React from "react";
-
+import './Navbar.css'
 const activeLink = { fontWeight: "bold", textDecoration: "underline" };
 const inActiveLink = { fontWeight: "normal" };
 
@@ -8,18 +8,23 @@ const Navbar = () => {
   return (
     <>
       <nav>
-        <NavLink
-          to="/"
-          style={({ isActive }) => (isActive ? activeLink : inActiveLink)}
-        >
-          Home
-        </NavLink>
-        <NavLink
-          to="/About"
-          style={({ isActive }) => (isActive ? activeLink : inActiveLink)}
-        >
-          About
-        </NavLink>
+        <div className="logo">
+            NetFlix | Budget Edition
+        </div>
+        <div className="nav-links">
+          <NavLink
+            to="/"
+            style={({ isActive }) => (isActive ? activeLink : inActiveLink)}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/About"
+            style={({ isActive }) => (isActive ? activeLink : inActiveLink)}
+          >
+            About
+          </NavLink>
+        </div>
       </nav>
     </>
   );
